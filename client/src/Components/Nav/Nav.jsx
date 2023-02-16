7c3285119626256114a8f393f117bfe8f3ae8c61
jnsengi <101756502+jnsengi@users.noreply.github.com>
2023-02-15 22:10:34 -0500

    Navigation links put into rounded boxes and creating user profile

--- client/src/Components/Nav/Nav.jsx
+++ client/src/Components/Nav/Nav.jsx
@@ -28,18 +28,20 @@ export const Nav = () => {
         </button>
       </div>
 
-      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div class="flex justify-end text-sm lg:flex-grow">
+      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto space-x-2">
+        <div class="flex justify-end space-x-2 text-sm lg:flex-grow">
           <a
             href="#responsive-header"
-            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            //"block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
           >
             STAR Category
           </a>
           <Link to={"/tips"}>
             <a
               href="#responsive-header"
-              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+              class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+              //"block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
             >
               Tips
             </a>
@@ -47,7 +49,8 @@ export const Nav = () => {
           <Link to={"/feedback"}>
             <a
               href="#responsive-header"
-              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+              class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+              //"block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
             >
               Feedback
             </a>
@@ -55,18 +58,21 @@ export const Nav = () => {
           <Link to={"/contact"}>
             <a
               href="#responsive-header"
-              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+              class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+              //"block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
             >
               Contact
             </a>
           </Link>
         </div>
+        
         <div>
           <a
             href="#"
-            class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full font-medium text-white dark:bg-gray-600  hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            
           >
-            Download
+            JN 
           </a>
         </div>
       </div>
