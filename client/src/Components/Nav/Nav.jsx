0a989ed01eaf915b609fb3fe7b81462204be50eb
Kyle Williams <kwilliamsdev98@gmail.com>
2023-02-20 19:09:42 -0500

    Get user data via auth0 api

--- client/src/Components/Nav/Nav.jsx
+++ client/src/Components/Nav/Nav.jsx
@@ -65,14 +65,13 @@ export const Nav = () => {
             </a>
           </Link>
         </div>
-        
+
         <div>
           <a
             href="#"
             class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full font-medium text-white dark:bg-gray-600  hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-            
           >
-            JN 
+            JN
           </a>
         </div>
       </div>
