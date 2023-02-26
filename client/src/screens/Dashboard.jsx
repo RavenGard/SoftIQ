41259a2bac9d1322d9d481c337036be7a2fdf97c
alanoli99 <alanoli99@gmail.com>
2023-02-26 06:31:50 -1000

    updated the dashboard to implement a video, score data and feedback info (placeHolder)
    Co-authored-by: RavenGard <RavenGard@users.noreply.github.com>
    Co-authored-by: Joe Nsengiyumva <jnsengiyumva1@ggc.edu>

--- client/src/screens/Dashboard.jsx
+++ client/src/screens/Dashboard.jsx
@@ -1,12 +1,34 @@
 import LoginButton from "../auth/SignIn";
 import LogoutButton from "../auth/SignOut";
+import SignuptButton from "../auth/SignUp";
 import Profile from "../Components/Profile/Profile";
+import demoData from "../assets/demoDataPic.jpg";
 
 export const Dashboard = () => {
   return (
     <div class="bg-gray-100 min-h-screen">
       <header class="bg-white shadow"></header>
       <main class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          <div class="aspect-w-16 aspect-h-9">
+            <iframe
+              width="1000"
+              height="500"
+              src="https://www.youtube.com/embed/kayOhGRcNt4"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </div>
+
+          <div class="bg-white rounded-lg shadow px-4 py-5 sm:p-6 mb-8">
+            <h2 class="text-lg font-medium leading-6 text-gray-900">
+              Track your scores and review your feedback!
+            </h2>
+            <img src={demoData} alt="score data and feedback" />
+          </div>
+        </div>
         <div class="bg-white rounded-lg shadow px-4 py-5 sm:p-6 mb-8">
           <h2 class="text-lg font-medium leading-6 text-gray-900">
             Interview Tip of the Day
@@ -31,12 +53,18 @@ export const Dashboard = () => {
         <div class="px-4 py-6 sm:px-0">
           <div class="flex justify-center items-center mb-4">
             <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-              <LoginButton>Log In</LoginButton>
+              <SignuptButton />
             </button>
           </div>
+          <div class="flex justify-center items-center mb-4">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+              <LoginButton />
+            </button>
+          </div>
+
           <div class="flex justify-center items-center mb-8">
             <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-              <LogoutButton>Log Out</LogoutButton>
+              <LogoutButton />
             </button>
           </div>
           <Profile />
