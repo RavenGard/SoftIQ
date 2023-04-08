5a7162be2dd3e790b175f30270a745978e23beb4
RavenGard <101682799+RavenGard@users.noreply.github.com>
2023-04-08 15:44:08 -0400

    made progress on recorder
    Co-authored-by: Alan Oliver Santiesteban <alanoli99@users.noreply.github.com>

--- client/src/screens/Interview.jsx
+++ client/src/screens/Interview.jsx
@@ -1,53 +1,98 @@
-import React from "react";
-import {
-  ReactMediaRecorder,
-  useReactMediaRecorder,
-} from "react-media-recorder";
-import { Link } from "react-router-dom";
+// https://stackblitz.com/edit/react-xjurfc?file=src%2FApp.js
+
+import React, { useRef, useState, useEffect } from "react";
+import { ReactMediaRecorder } from "react-media-recorder";
+
+const VideoPreview = ({ stream }) => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current && stream) {
+      videoRef.current.srcObject = stream;
+    }
+  }, [stream]);
+
+  if (!stream) {
+    return null;
+  }
+
+  return <video ref={videoRef} width={500} height={500} autoPlay controls />;
+};
 
 export const Interview = () => {
-  const { status, startRecording, stopRecording, mediaBlobUrl } =
-    useReactMediaRecorder({ video: true });
+  const [enable, setEnable] = useState(true);
+  const [recorded, setRecorded] = useState(false);
 
   const handleRecordingComplete = (blob) => {
     console.log("recording complete", blob);
+    setRecorded(true);
+  };
+
+  const handleEraseVideo = () => {
+    setRecorded(false);
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      {status === "idle" && (
-        <button
-          className="py-4 px-8 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition-colors duration-300"
-          onClick={startRecording}
-        >
-          Start Interview
-        </button>
-      )}
-      {status === "recording" && (
-        <div className="w-full h-full">
-          <video
-            className="w-full h-full object-contain"
-            src={mediaBlobUrl}
-            controls
-          />
-        </div>
-      )}
-      {status === "recording" && (
-        <button
-          className="py-4 px-8 bg-red-500 text-white rounded-full hover:bg-red-600 transition-colors duration-300"
-          onClick={stopRecording}
-        >
-          Stop Interview
-        </button>
-      )}
+    <div className="flex flex-col items-center justify-center h-full w-full">
       <ReactMediaRecorder
         video
         onRecordingComplete={handleRecordingComplete}
-        render={({ status, startRecording, stopRecording }) => <div></div>}
+        render={({
+          previewStream,
+          status,
+          startRecording,
+          stopRecording,
+          mediaBlobUrl,
+        }) => {
+          return (
+            <div className="max-w-md mx-auto my-8">
+              {mediaBlobUrl ? (
+                <video
+                  className="block mx-auto mb-4"
+                  src={mediaBlobUrl}
+                  controls
+                  autoPlay
+                  loop
+                />
+              ) : (
+                <div />
+                // <div className="block mb-4 w-full h-full bg-gray-200" />
+              )}
+
+              {status !== "recording" && recorded === false && (
+                <button
+                  className="block mx-auto mb-4 px-10 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition-colors duration-300"
+                  onClick={startRecording}
+                >
+                  Start Recording
+                </button>
+              )}
+
+              {enable && <VideoPreview stream={previewStream} />}
+
+              {status === "recording" && (
+                <div>
+                  <button
+                    className="block mx-auto mb-4 px-4 py-2 bg-red-500 text-white rounded-full hover:bg-red-600 transition-colors duration-300"
+                    onClick={stopRecording}
+                  >
+                    Stop Recording
+                    {setRecorded(true)}
+                  </button>
+                </div>
+              )}
+
+              {recorded && (
+                <button 
+                className="block mx-auto mb-4 px-4 py-2 bg-gray-500 text-white rounded-full hover:bg-gray-600 transition-colors duration-300"
+                onClick={startRecording}>
+                  Try Again
+                </button>
+              )}
+            </div>
+          );
+        }}
       />
-      <Link to="/" className="mt-8 underline">
-        Back to Dashboard
-      </Link>
     </div>
   );
 };
--- client/src/screens/LoggedInDash.jsx
+++ client/src/screens/LoggedInDash.jsx
@@ -3,8 +3,15 @@ import { Link } from "react-router-dom";
 
 export const LoggedInDash = () => {
   return (
-    <div>
-      <Link to="/interview">Go to Interview</Link>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white shadow-md rounded-lg p-6 w-full max-w-md">
+        <Link
+          to="/interview"
+          className="block w-full text-center py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 transition-colors duration-300"
+        >
+          Go to Interview
+        </Link>
+      </div>
     </div>
   );
 };
