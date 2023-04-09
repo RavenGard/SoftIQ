7a415bd9a95261d35dcb0981b0a5f2103793f15f
alanoli99 <alanoli99@gmail.com>
2023-04-09 11:28:21 -0400

    fixed the issue where when you pressed try again you only get one screen instead of two. Also fixed the issue where the previous recorded video will be cleared so that the user may see themself again when they restart the recording
    Co-authored-by: RavenGard <RavenGard@users.noreply.github.com>
    Co-authored-by: Kyle Williams <KWilliams-dev@users.noreply.github.com>
    Co-authored-by: Joe Nsengiyumva <jnsengiyumva1@ggc.edu>

--- client/src/screens/Interview.jsx
+++ client/src/screens/Interview.jsx
@@ -1,38 +1,57 @@
+//https://stackblitz.com/edit/react-xjurfc?file=src%2FApp.js
 import React, { useRef, useState, useEffect } from "react";
 import { ReactMediaRecorder } from "react-media-recorder";
 
-// This component is responsible for showing a video preview of the stream
+// The VideoPreview component is responsible for displaying a video preview of the given stream
 const VideoPreview = ({ stream }) => {
+  // videoRef is a reference to the video DOM element
   const videoRef = useRef(null);
+  //updates the video element's srcObject property
+  // when the stream changes. It accepts a callback function that runs when the component mounts or when the
+  // specified dependencies (in this case, the stream) change
 
-  // When the stream changes, we update the video element's srcObject
   useEffect(() => {
+    // If there is a video DOM element (videoRef.current) and a stream, update the video element's srcObject property
+    // with the stream. This ensures that the video preview displays the correct stream.
     if (videoRef.current && stream) {
       videoRef.current.srcObject = stream;
     }
   }, [stream]);
-
-  // If there's no stream, don't show the video element
+  // If there's no stream, the VideoPreview component returns null, which means nothing will be rendered
   if (!stream) {
     return null;
   }
 
+  // If there is a stream, render a video element with the provided reference (videoRef), width, height, autoPlay, and
+  // controls properties. The autoPlay property ensures that the video preview starts playing automatically, and the
+  // controls property adds playback controls to the video element.
   return <video ref={videoRef} width={500} height={500} autoPlay controls />;
 };
 
 export const Interview = () => {
+  // recorded is a state variable that indicates whether a video has been recorded or not. setRecorded is a function
+  // to update the recorded state.
   const [recorded, setRecorded] = useState(false);
+  const [mediaUrl, setMediaUrl] = useState(null);
 
   const handleRecordingComplete = (blob) => {
     console.log("recording complete", blob);
     setRecorded(true);
+    const myURL = new URL("http://localhost:5173/interview");
+    setMediaUrl(myURL.createObjectURL(blob));
   };
 
+  // The Interview component returns a container div with the ReactMediaRecorder component inside it.
   return (
     <div className="flex flex-col items-center justify-center h-full w-full">
       <ReactMediaRecorder
+        // The video prop indicates that the recording will be a video.
         video
+        // onRecordingComplete is a callback function that is called when the recording is completed.
         onRecordingComplete={handleRecordingComplete}
+        // The render prop is a function that returns the UI elements (buttons and video previews) based on the
+        // recording status and the recorded video URL (mediaBlobUrl). This function receives an object containing
+        // the following properties: previewStream, status, startRecording, stopRecording, and mediaBlobUrl.
         render={({
           previewStream,
           status,
@@ -42,7 +61,7 @@ export const Interview = () => {
         }) => {
           return (
             <div className="max-w-md mx-auto my-8">
-              {/* Show the recorded video if it exists, otherwise show an empty div 
+              {/* /* Show the recorded video if it exists, otherwise show an empty div 
               
               mediaBlobUrl is a property provided by the ReactMediaRecorder component. It 
               represents a URL that points to the recorded video data in the browser's memory. 
@@ -54,7 +73,7 @@ export const Interview = () => {
               element for better user experience.If mediaBlobUrl is null or undefined (i.e., no video has been 
                 recorded yet), it renders an empty div instead.This conditional rendering is done to 
                 show the recorded video when it's available and hide it when it's not.*/}
-              {mediaBlobUrl ? (
+              {recorded ? (
                 <video
                   className="block mx-auto mb-4"
                   src={mediaBlobUrl}
@@ -63,7 +82,7 @@ export const Interview = () => {
                   loop
                 />
               ) : (
-                <div />
+                <VideoPreview stream={previewStream} />
               )}
 
               {/* Show the "Start Recording" button if not recording and no video recorded */}
@@ -76,9 +95,6 @@ export const Interview = () => {
                 </button>
               )}
 
-              {/* Show the video preview */}
-              <VideoPreview stream={previewStream} />
-
               {/* Show the "Stop Recording" button if currently recording */}
               {status === "recording" && (
                 <div>
@@ -87,6 +103,7 @@ export const Interview = () => {
                     onClick={() => {
                       stopRecording();
                       setRecorded(true);
+                      handleRecordingComplete();
                     }}
                   >
                     Stop Recording
@@ -100,6 +117,7 @@ export const Interview = () => {
                   className="block mx-auto mb-4 px-4 py-2 bg-gray-500 text-white rounded-full hover:bg-gray-600 transition-colors duration-300"
                   onClick={() => {
                     setRecorded(false);
+                    setMediaUrl(null);
                   }}
                 >
                   Try Again
