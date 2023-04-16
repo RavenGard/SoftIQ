ad3d8a1f5db27637c04b6cba11b977318a2d06ef
Kyle Williams <kwilliamsdev98@gmail.com>
2023-04-15 20:01:55 -0400

    Fixed blank screen from empty bar chart

--- client/src/Components/Charts/BarChart.jsx
+++ client/src/Components/Charts/BarChart.jsx
@@ -3,58 +3,17 @@ import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 import authContext from "../../context/auth-context";
 import { useState, useContext } from "react";
+import Oops from "../../screens/Oops";
+
+// Moved calcAverageWeeklyScore to ChartHelper.js
+import { calcAverageWeeklyScore } from "./ChartHelper";
 
 const BarChart = () => {
   const context = useContext(authContext);
 
   const userId = context.userId;
 
-  const [feedback, setFeedback] = useState([
-    { user: { firstName: "" }, month: "", category: "", score: 0, week: "" },
-  ]);
-
-  const calcAverageWeeklyScore = (feedback) => {
-    let week1Sum = 0;
-    let week2Sum = 0;
-    let week3Sum = 0;
-    let week4Sum = 0;
-    let week1Avg = 0;
-    let week2Avg = 0;
-    let week3Avg = 0;
-    let week4Avg = 0;
-    let week1Count = 0;
-    let week2Count = 0;
-    let week3Count = 0;
-    let week4Count = 0;
-
-    feedback.forEach((element) => {
-      if (element.week === "Week 1") {
-        week1Sum += element.score;
-        console.log("This is the element.score: " + element.score);
-        week1Count++;
-      } else if (element.week === "Week 2") {
-        week2Sum += element.score;
-        week2Count++;
-      } else if (element.week === "week 3") {
-        week3Sum += element.score;
-        week3Count++;
-      } else if (element.week === "week 4") {
-        week4Sum += element.score;
-        week4Count++;
-      }
-    });
-    week1Avg = week1Sum / week1Count;
-    week2Avg = week2Sum / week2Count;
-    week3Avg = week3Sum / week3Count;
-    week4Avg = week4Sum / week4Count;
-
-    console.log("Week 1 Avg: " + week1Avg);
-    console.log("Week 2 Avg: " + week2Avg);
-    console.log("Week 3 Avg: " + week3Avg);
-    console.log("Week 4 Avg: " + week4Avg);
-
-    return [week1Avg, week2Avg, week3Avg, week4Avg];
-  };
+  const [feedback, setFeedback] = useState([]);
 
   useEffect(() => {
     //TODO: Data could not be retrieved because we were trying to set single values to an array. We need to set the entire array to the state.
@@ -90,29 +49,39 @@ const BarChart = () => {
         return res.json();
       })
       .then((resData) => {
-        setFeedback(
-          resData.data.getAllFeedback.filter(
-            (data) => data.category === "Eye Contact"
-          )
-        );
-        console.log("This is the feedback: " + feedback);
+        // This tests if the user has created any feedback before we set the state of the feedback array.
+        // Otherwise, we get an error when we try to access the first element of the array.
+        if (resData.data.getAllFeedback.length > 0) {
+          setFeedback(
+            resData.data.getAllFeedback.filter(
+              (data) => data.category === "Eye Contact"
+            )
+          );
+        } else {
+          console.log("No feedback yet!");
+          console.log();
+        }
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
-  const data = {
-    datasets: [
-      {
-        label: `${feedback[0].user.firstName}'s ${feedback[0].month} ${feedback[0].category} Feedback`,
-        data: calcAverageWeeklyScore(feedback),
-        fill: false,
-        backgroundColor: "rgb(75, 192, 192)",
-        borderColor: "rgba(75, 192, 192, 0.2)",
-      },
-    ],
-  };
+  let data;
+
+  if (feedback.length > 0) {
+    data = {
+      datasets: [
+        {
+          label: `${feedback[0].user.firstName}'s ${feedback[0].month} ${feedback[0].category} Feedback`,
+          data: calcAverageWeeklyScore(feedback),
+          fill: false,
+          backgroundColor: "rgb(75, 192, 192)",
+          borderColor: "rgba(75, 192, 192, 0.2)",
+        },
+      ],
+    };
+  }
 
   const options = {
     scales: {
@@ -126,43 +95,11 @@ const BarChart = () => {
     },
   };
 
-  return <Bar data={data} options={options} />;
+  return (
+    <>
+      {feedback.length !== 0 ? <Bar data={data} options={options} /> : <Oops />}
+    </>
+  );
 };
-
-// import React, { useState } from "react";
-// import { Bar } from "react-chartjs-2";
-
-//
-
-// const ChartWithReport = ({ data, options }) => {
-//   const [selectedData, setSelectedData] = useState(null);
-
-//
-
-//   const handleClick = (event) => {
-//     const { datasetIndex, index } = event;
-//     const clickedData = data.datasets[datasetIndex].data[index];
-//     setSelectedData(clickedData);
-//   };
-
-//
-
-//   return (
-// <div>
-// <Bar data={data} options={options} onClick={handleClick} />
-//       {selectedData && <Report data={selectedData} />}
-// </div>
-//   );
-// };
-
-//
-
-// const Report = ({ data }) => {
-//   // render the report with the selected data
-// };
-
-//
-
-// export default ChartWithReport;
 
 export default BarChart;
